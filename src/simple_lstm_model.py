bdb3c6ac80f57ca453866955a9f5746e0eb1df6a
Lily W <lilycwei2027@gmail.com>
2026-04-27 16:09:17 -0400

    add simple models

---
+++ src/simple_lstm_model.py
@@ -0,0 +1,22 @@
+# example from here: https://www.geeksforgeeks.org/deep-learning/long-short-term-memory-networks-using-pytorch/
+
+import torch
+from torch import nn
+
+
+class SimpleLSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(SimpleLSTMModel, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x, h0=None, c0=None):
+        if h0 is None or c0 is None:
+            h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
+            c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).to(x.device)
+
+        out, (hn, cn) = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
